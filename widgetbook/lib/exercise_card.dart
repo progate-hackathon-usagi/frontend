82f74d2031d2fce7f6b475103aae754721cd3099
yotu <114195789+ulxsth@users.noreply.github.com>
2024-08-24 21:49:02 +0900

    feat: いい感じにスタイリング

--- widgetbook/lib/exercise_card.dart
+++ widgetbook/lib/exercise_card.dart
@@ -16,36 +16,61 @@ class ExerciseCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('$year年 $month月', style: const TextStyle(fontSize: 36)),
-            GridView.builder(
-              shrinkWrap: true, // 高さをコンテンツに合わせる
-              physics: const NeverScrollableScrollPhysics(),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 7,
-              ),
-              itemCount: isExercisedList.length,
-              itemBuilder: (context, index) {
-                DateTime date = DateTime(year, month, index + 1);
-                bool isDone = isExercisedList[index];
+    const textColor = Colors.white;
+    const headingTextStyle = TextStyle(fontSize: 36, color: textColor);
+    const calendarTextStyle = TextStyle(fontSize: 16, color: textColor);
 
-                return GridTile(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text('${date.day}日'),
-                      Text(isDone ? '済' : ''),
-                    ],
+    return SizedBox(
+      width: 450,
+      height: 600,
+      child: Card(
+        color: Colors.teal[200],
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Card(
+            color: Colors.teal[400],
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text('$year年 $month月', style: headingTextStyle),
+                  const SizedBox(height: 20),
+                  Card(
+                    color: const Color.fromRGBO(77, 182, 172, 1),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 50, horizontal: 10),
+                      child: GridView.builder(
+                        shrinkWrap: true, // 高さをコンテンツに合わせる
+                        physics: const NeverScrollableScrollPhysics(),
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 7,
+                        ),
+                        itemCount: isExercisedList.length,
+                        itemBuilder: (context, index) {
+                          DateTime date = DateTime(year, month, index + 1);
+                          bool isDone = isExercisedList[index];
+
+                          return GridTile(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text('${date.day}日', style: calendarTextStyle),
+                                Text(isDone ? '済' : '',
+                                    style: calendarTextStyle),
+                              ],
+                            ),
+                          );
+                        },
+                      ),
+                    ),
                   ),
-                );
-              },
+                ],
+              ),
             ),
-          ],
+          ),
         ),
       ),
     );
