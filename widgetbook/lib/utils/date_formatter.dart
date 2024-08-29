ee17fc6510fffe0135f3d2e63bf8e28e20b79350
urassh <urassh.1011@gmail.com>
2024-08-29 12:29:15 +0900

    feat: コメントで使用例を書いた

--- widgetbook/lib/utils/date_formatter.dart
+++ widgetbook/lib/utils/date_formatter.dart
@@ -1,6 +1,7 @@
 import 'package:intl/intl.dart';
 
 class DateFormatter {
+  // 例: "AM 6:27"
   static String formatTime(DateTime dateTime) {
     final DateFormat formatter = DateFormat('a h:mm');
     return formatter.format(dateTime);
