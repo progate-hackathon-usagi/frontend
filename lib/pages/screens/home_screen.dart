d1000c9092deb97e377ea00dcfdbd0409271baf8
ulxsth <ulxsth@gmail.com>
2024-08-31 05:14:32 +0900

    refactor: lobby,profileをTabBarWidgetに移植

--- lib/pages/screens/home_screen.dart
+++ lib/pages/screens/home_screen.dart
@@ -1,16 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final supabase = Supabase.instance.client;
+    const headingTextStyle = TextStyle(fontSize: 36, color: Colors.black);
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home'),
+        actions: [
+          IconButton(
+            iconSize: 50,
+            icon: Icon(Icons.logout),
+            onPressed: () {
+              supabase.auth.signOut();
+              Navigator.pushNamed(context, '/signin');
+            },
+          ),
+        ],
       ),
-      body:
-          const Center(child: Text('Home画面', style: TextStyle(fontSize: 32.0))),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {},
+          child: const Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('ラジオ体操に', style: headingTextStyle),
+              Text("参加する", style: headingTextStyle),
+              SizedBox(height: 20),
+              Text('212人が参加中...'),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/pages/screens/profile_screen.dart
+++ lib/pages/screens/profile_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatelessWidget {
-  const ProfileScreen({Key? key}) : super(key: key);
+  const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -10,7 +10,41 @@ class ProfileScreen extends StatelessWidget {
         title: const Text('Profile'),
       ),
       body: const Center(
-          child: Text('プロフィール画面', style: TextStyle(fontSize: 32.0))),
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 100,
+                backgroundImage: NetworkImage(
+                    'https://via.placeholder.com/350x350?text=sample'),
+              ),
+              Text("おじ",
+                  style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold)),
+              Expanded(
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const <Widget>[
+                      Text(
+                        '累計：785 日',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      Text(
+                        '連続：420 日',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      // 他のテキストウィジェットを追加
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
