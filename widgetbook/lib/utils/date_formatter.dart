dbba6ffab7fba54613a0fae9f61fa2c557efe449
urassh <urassh.1011@gmail.com>
2024-08-31 11:04:32 +0900

    delete: widgetbookにあったdate_formatterを削除した

--- widgetbook/lib/utils/date_formatter.dart
+++
@@ -1,9 +0,0 @@
-import 'package:intl/intl.dart';
-
-class DateFormatter {
-  // 例: "AM 6:27"
-  static String formatTime(DateTime dateTime) {
-    final DateFormat formatter = DateFormat('a h:mm');
-    return formatter.format(dateTime);
-  }
-}
