0b43c83c47100cadf4547f003292b3cc303b124a
rare0b <rare0b0110@gmail.com>
2024-09-01 12:31:53 +0900

    feat: Refactor date calculations and add exercise logs to calendar view

--- lib/utils/date_time_ext.dart
+++ lib/utils/date_time_ext.dart
@@ -1,6 +1,6 @@
 extension DateTimeExt on DateTime {
   DateTime get yesterday => subtract(const Duration(days: 1));
   DateTime get tomorrow => add(const Duration(days: 1));
-  DateTime get previousMonth => subtract(const Duration(days: 31));
-  DateTime get nextMonth => add(const Duration(days: 31));
+  DateTime get previousMonth => DateTime(year, month - 1, day);
+  DateTime get nextMonth => DateTime(year, month + 1, day);
 }
